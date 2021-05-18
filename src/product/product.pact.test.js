7256765b41d79510e5dd6e155ce8940ff51119f9
thanh.nguyen <thanh.nguyen@bstock.com>
2021-05-18 14:35:19 +0700

    Implement pact test

---
+++ src/product/product.pact.test.js
@@ -0,0 +1,56 @@
+const { Verifier } = require('@pact-foundation/pact');
+const controller = require('./product.controller');
+const Product = require('./product');
+
+const url = 'http://localhost:';
+const port = 8080;
+
+// Setup provider server to verify
+const app = require('express')();
+app.use(require('./product.routes'));
+const server = app.listen(port);
+
+describe("Pact Verification", () => {
+    it("Validates the expectations of ProductService", () => {
+        const baseOpts = {
+            logLevel: "INFO",
+            providerBaseUrl: `${url + port}`,
+            providerVersion: '1.0.0',
+            provider: "Provider",
+            pactBrokerUrl: process.env.PACT_BROKER_URL,
+            pactBrokerToken: process.env.PACT_BROKER_TOKEN,
+            publishVerificationResult: true
+        }
+
+        const stateHandlers = {
+            "products exist": () => {
+                controller.repository.products = new Map([
+                    ["10", new Product("10", "CREDIT_CARD", "28 Degrees")]
+                ]);
+            },
+        }
+
+        const requestFilter = (req, res, next) => {
+            if (!req.headers["authorization"]) {
+                next();
+                return;
+            }
+            req.headers["authorization"] = `Bearer ${new Date().toISOString()}`;
+            next();
+        }
+
+        const opts = {
+            ...baseOpts,
+            stateHandlers: stateHandlers,
+            requestFilter: requestFilter
+        };
+
+        return new Verifier(opts).verifyProvider()
+            .then(output => {
+                console.log("Pact Verification Complete!")
+            })
+            .finally(() => {
+                server.close();
+            });
+    })
+});
